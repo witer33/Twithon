0a3db36b7b4dc623177a03dad140da2dc872a57a
witer33 <leox337@gmail.com>
2020-04-25 16:09:59 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'Twithon',
+  packages = ['Twithon'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'A python wrapper over Twitch API to build bots and applications.',
+  author = 'Witer33',
+  author_email = 'leox337@gmail.com',
+  url = 'witer33.com',
+  download_url = 'https://github.com/witer33/Twithon/archive/v_0.0.1.tar.gz',
+  keywords = ['Twitch', 'chatbot', 'api'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
