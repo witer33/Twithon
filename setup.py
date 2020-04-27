4f22f888b8324340a9d8247c3c72d75ec15958e7
witer33 <leox337@gmail.com>
2020-04-27 13:05:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'twithon',         # How you named your package folder (MyLib)
   packages = ['twithon'],   # Chose the same as "name"
-  version = '0.0.2',      # Start with a small number and increase it with every change you make
+  version = '0.0.2.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A python wrapper over Twitch API to build bots and applications.',   # Give a short description about your library
   author = 'Witer33',                   # Type in your name
