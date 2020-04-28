8fbd687b2d3fb19ed82396d8a3318615ef274958
witer33 <leox337@gmail.com>
2020-04-28 09:48:18 +0200

    Create api.py

---
+++ twithon/api.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+class client:
+
+    def __init__(self, client_id):
+        self.client_id = client_id
+
+    def get(self, url, data):
+        header = {"Client-ID": str(self.client_id)}
+        req = requests.get("https://api.twitch.tv/helix/{}".format(url), params=data, headers=header)
+        return req.json()
+
+    def __getattr__(self, item):
+        item = item.replace("_", "/")
+        return (lambda data : self.get(item, data))
